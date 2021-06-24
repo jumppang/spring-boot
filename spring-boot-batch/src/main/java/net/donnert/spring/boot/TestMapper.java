84d8557959ab5f0e64426a3bb0a668ea68da476a
jusu <jusu@DESKTOP-IRBB8A7>
2021-06-24 19:42:52 +0900

    dddddfff

--- spring-boot-batch/src/main/java/net/donnert/spring/boot/TestMapper.java
+++ spring-boot-batch/src/main/java/net/donnert/spring/boot/TestMapper.java
@@ -1,5 +1,5 @@
 package net.donnert.spring.boot;
 
-public interface TestMapper {
+public interface TestMapper { 
 	public String getValueFromDatabase();
 }
---
+++ spring-boot-batch/src/main/java/net/donnert/spring/boot/sddd.java
@@ -0,0 +1,5 @@
+package net.donnert.spring.boot;
+
+public class sddd {
+
+}
